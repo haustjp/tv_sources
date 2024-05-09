e9153a9d627707988ba2bd2bfd7ede58c7b934ec
hausthy <test@example.com>
2024-05-09 20:51:49 +0800

    auto

--- get_iptv_source.py
+++ get_iptv_source.py
@@ -1,7 +1,101 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs
+from logging.handlers import TimedRotatingFileHandler
+from logging import Logger
 import json
 import requests
 import re
+import os
+import sys
+import logging
+
+
+province_dict = [{
+    'province_name': '广东',
+    'province_code': 'guangdong_iptv'
+}, {
+    'province_name': '北京',
+    'province_code': 'beijing_iptv'
+}, {
+    'province_name': '四川',
+    'province_code': 'sichuan_iptv'
+}, {
+    'province_name': '河南',
+    'province_code': 'henan_iptv'
+}]
+
+
+logger: Logger = None
+
+
+def init_logger(logPath: str):
+    # 日志输入文件
+    # handler = logging.FileHandler(logPath, 'a', 'utf-8')
+    folder_name = os.path.dirname(logPath)
+    os.makedirs(folder_name, exist_ok=True)
+    handler = TimedRotatingFileHandler(
+        logPath, when='D', backupCount=7, encoding='utf-8')
+    logging.basicConfig(handlers=[handler], level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(thread)d - %(message)s')
+    logger = logging.getLogger(__name__)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(thread)d - %(message)s')
+    streamHandler = logging.StreamHandler(sys.stdout)
+    streamHandler.setFormatter(formatter)
+    # logger.addHandler(handler)
+    logger.addHandler(streamHandler)
+    return logger
+
+
+def query_by_province(province, page=None, code=None):
+    query_result = {}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Referer': f'http://tonkiang.us/hoteliptv.php',
+        'Cookie': 'REFERER=15239974; ckip1=110.72.82.168%7C110.72.81.102%7C113.118.47.23%7C14.155.189.170%7C112.114.137.112%7C61.134.241.142%7C61.138.128.226%7C113.64.145.231; ckip2=183.153.89.133%7C218.88.103.176%7C58.48.199.92%7C222.141.135.44%7C171.109.211.31%7C118.79.112.201%7C118.113.235.79%7C223.10.39.217; _ga=GA1.1.1545478787.1715258144; HstCfa4853344=1715258161531; HstCmu4853344=1715258161531; HstCnv4853344=1; HstCns4853344=1; REFERER2=NzDbAr2aNbjcIO0O0O; REFERER1=MzjbMryaNbTcUO0O0O; HstCla4853344=1715258167679; HstPn4853344=2; HstPt4853344=2; _ga_JNMLRB3QLF=GS1.1.1715258144.1.1.1715258176.0.0.0',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {'saerch': province, 'Submit': None}
+    response = requests.post(
+        f'http://tonkiang.us/hoteliptv.php', data=data, headers=headers)
+
+    html = response.text
+    if html is None or len(html) == 0:
+        return query_result
+    bs = BeautifulSoup(html, 'html.parser')
+    pagers = bs.select('a[href^="?page="]')
+
+    if pagers and len(pagers) > 0:
+        href = pagers[0].attrs['href']
+        parsed_url = urlparse(href)
+        query_params = parse_qs(parsed_url.query)
+        code = query_params['code'][0]
+        query_result['url'] = href
+        query_result['code'] = code
+
+    tables = bs.select('div[class="tables"]')
+    logger.info(f'{province}_tables长度：{len(tables)}')
+    sources = []
+
+    for table in tables:
+        results = table.select('div[class="result"]')
+        for result in results:
+            channel = result.find('div', class_='channel')
+            if channel:
+                ip_port = channel.b.text.strip()
+                active_tag = result.find('div', style='float: right; ')
+                active_text = active_tag.text.replace("\n", "").strip()
+                if active_text != '暂时失效':
+                    logger.info(f'status：{active_text}')
+                    logger.info(f'channel：{ip_port}')
+                    sources.append(ip_port)
+
+    query_result['sources'] = sources
+
+    logger.info(f'{province}可用直播组:{json.dumps(query_result)}')
+
+    return query_result
 
 
 def get_html_source(host_url):
@@ -20,39 +114,43 @@ def get_html_source(host_url):
 
 
 def get_channel_sources(html):
-    bs = BeautifulSoup(html, 'html.parser')
+    try:
+        bs = BeautifulSoup(html, 'html.parser')
 
-    # 获取直播源名称
-    channel_name = 'data'
-    channel_results = bs.select('div[class="result"]')
+        # 获取直播源名称
+        channel_name = 'data'
+        channel_results = bs.select('div[class="result"]')
 
-    if channel_results is not None and len(channel_results) > 0:
-        channel_name = channel_results[0].b.text.replace(':', '_')
+        if channel_results is not None and len(channel_results) > 0:
+            channel_name = channel_results[0].b.text.replace(':', '_')
 
-    tables = bs.select('div[class="tables"]')
-    print(f'tables长度：{len(tables)}')
+        tables = bs.select('div[class="tables"]')
+        logger.info(f'直播源_tables长度：{len(tables)}')
 
-    sources = []
+        sources = []
 
-    for table in tables:
-        results = table.select('div[class="result"]')
-        # print(f'channels长度：{len(results)}')
-        for result in results:
-            channels = result.select('.channel')
-            if channels is None or len(channels) <= 0:
-                continue
-            name = channels[0].div.text.replace('\n', '')
-            print(f'channel_name：{name}')
-            m3u8 = result.select('.m3u8')[0]
-            url = m3u8.select('td')[1].text.strip()
-            print(f'channel_url：{url}')
+        for table in tables:
+            results = table.select('div[class="result"]')
+            #  logger.info(f'channels长度：{len(results)}')
+            for result in results:
+                channels = result.select('.channel')
+                if channels is None or len(channels) <= 0:
+                    continue
+                name = channels[0].div.text.replace('\n', '')
+                # logger.info(f'channel_name：{name}')
+                m3u8 = result.select('.m3u8')[0]
+                url = m3u8.select('td')[1].text.strip()
+                # logger.info(f'channel_url：{url}')
 
-            sources.append({
-                'name': name,
-                'url': url
-            })
-
-    return channel_name, sources
+                sources.append({
+                    'name': name,
+                    'url': url
+                })
+        logger.info(f'{channel_name}:{json.dumps(sources)}')
+        return channel_name, sources
+    except:
+        logger.info(html)
+        return None, None
 
 
 def sort_key(item):
@@ -149,7 +247,7 @@ def build_txt_file(channel_name, dict_sources):  # 保存txt数据
             file.write(txt_string)
 
 
-def build_m3u8_file(channel_name, sources):  # 保存m3u8数据
+def build_m3u8_file(channel_name, dict_sources):  # 保存m3u8数据
     if dict_sources is not None and len(dict_sources) > 0:
         m3u8_string = '#EXTM3U\n'
         for key, value in dict_sources.items():
@@ -160,8 +258,38 @@ def build_m3u8_file(channel_name, sources):  # 保存m3u8数据
             file.write(m3u8_string)
 
 
+def get_channel_sources_by_province(province):
+    province_name = province['province_name']
+    province_code = province['province_code']
+    query_result = query_by_province(province_name)
+
+    if query_result is None or not 'sources' in query_result.keys():
+        return
+
+    sources = query_result['sources']
+
+    if sources is None or len(sources) == 0:
+        return
+    province_channel_sources = []
+    for source in query_result['sources']:
+        html = get_html_source(source)
+        channel_name, channel_sources = get_channel_sources(html)
+        if channel_sources is None:
+            continue
+        province_channel_sources.extend(channel_sources)
+
+    dict_sources = build_channel_sources(province_channel_sources)
+    build_json_file(province_code, dict_sources)
+    build_txt_file(province_code, dict_sources)
+    build_m3u8_file(province_code, dict_sources)
+
+
 if __name__ == "__main__":
     host_url = '221.220.108.96:4000'
+    logger = init_logger('logs/tv_sources.log')
+    for province in province_dict:
+        get_channel_sources_by_province(province)
+    exit(0)
     html = get_html_source(host_url)
     channel_name, channel_sources = get_channel_sources(html)
     dict_sources = build_channel_sources(channel_sources)
