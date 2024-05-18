364b84fdfed09ed3a47841b1d93d67af21b57936
hausthy <test@example.com>
2024-05-18 22:21:25 +0800

    sh

--- docker/auto_run_iptv.sh
+++ docker/auto_run_iptv.sh
@@ -4,6 +4,9 @@ function main(){
    cd /scripts/tv_sources
    git pull
    python3 get_iptv_source.py -i '广东' -o 'guangdong'
+   python3 get_iptv_source.py -i '北京' -o 'beijing'
+   python3 get_iptv_source.py -i '河南' -o 'henan'
+   python3 get_iptv_source.py -i '四川' -o 'sichuan'
 }
 
 function commit(){
