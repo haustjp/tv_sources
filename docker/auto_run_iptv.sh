8d92049a56343bd10d67dc7dd1b69589f020393e
hausthy <test@example.com>
2025-01-16 20:55:41 +0800

    aa

--- docker/auto_run_iptv.sh
+++ docker/auto_run_iptv.sh
@@ -3,12 +3,13 @@
 function main(){
    cd /scripts/tv_sources
    git pull
-   python3 get_iptv_source.py -i '广东' -o 'guangdong'
-   python3 get_iptv_source.py -i '北京' -o 'beijing'
-   python3 get_iptv_source.py -i '河南' -o 'henan'
-   python3 get_iptv_source.py -i '四川' -o 'sichuan'
-   python3 get_iptv_source.py -t 'first'
+   python3 get_iptv.py
+#    python3 get_iptv_source.py -i '北京' -o 'beijing'
+#    python3 get_iptv_source.py -i '河南' -o 'henan'
+#    python3 get_iptv_source.py -i '四川' -o 'sichuan'
+#    python3 get_iptv_source.py -t 'first'
    cp -rf /scripts/tv_sources/sources/* /scripts/iptv/
+   rm -rf /scripts/tv_sources/sources/* 
 }
 
 function commit(){
