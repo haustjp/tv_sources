e00255a98ca88941ffdc3e01789bd47f465f99b3
hausthy <test@example.com>
2025-05-27 10:52:19 +0800

    do_sign

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -3,3 +3,6 @@
 
 # IPTV自动抓取脚本
 30 0 * * * sh /scripts/custom/auto_run_iptv.sh >>/scripts/logs/auto_run_iptv.log 2>&1
+
+
+15 8 * * * python /scripts/custom/sign_task.py >>/scripts/logs/sign_task.log 2>&1
